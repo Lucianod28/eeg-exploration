9d27d46791fa3a7fc254b9ae88fab1a9a42a95d9
Lucianod28 <Lucianod28@gmail.com>
2018-12-12 14:28:48 -0800

    working on classification

--- plots.py
+++ plots.py
@@ -15,10 +15,35 @@ for line in lines[1:]:
 df = pd.read_csv('SubjectMichelle___timestamp_1501862371.27/SubjectMichelle__eeg.csv', header=8)
 # df2 = pd.read_csv('SubjectMichelle___timestamp_1501862954.25/SubjectMichelle__eeg.csv', header=8)
 # df3 = pd.read_csv('SubjectMichelle___timestamp_1501863670.92/SubjectMichelle__eeg.csv', header=8)
-# Create a list of all the left light trials, next I will include the other 2 recordings above
-lefts = []
+
+df['class'] = [0] * len(df.index)
+
+# Label left intervals with -1, right with 1, and rest with 0
 for trial in trials:
-	lefts.append(df.loc[(df['\t'] >= trial[0]) & (df['\t'] <= trial[1])])
-trial1_left = lefts[0]
-plt.plot(trial1_left['\t'], trial1_left['\t[\'Fp1\''])
-plt.show()
+	conditions = [(df['\t'] >= trial[0]) & (df['\t'] <= trial[1]), # left
+	(df['\t'] >= trial[2]) & (df['\t'] <= trial[3])] # right
+	choices = [-1, 1]
+	df['class'] = np.select(conditions, choices, default=0)
+
+# plt.plot(trial1_left['\t'], trial1_left['\t[\'Fp1\''])
+# plt.show()
+
+# Train an svm
+df.drop(['Channel Names'], 1, inplace=True) # drop first
+print(df.head())
+exit(0)
+X = np.array(df.drop(['class'], 1))
+y = np.array(df['class'])
+
+X_train, X_test, yTrain, yTest = model_selection.train_test_split(X, y, test_size=0.2)
+
+clf = svm.SVC()
+clf.fit(X_train, yTrain)
+
+accuracy = clf.score(X_test, yTest)
+print(accuracy)
+
+# example_measures = np.array([[4,2,1,1,1,2,3,2,1], [4,2,1,2,2,2,3,2,1]])
+# example_measures = example_measures.reshape(len(example_measures), -1)
+# prediction = clf.predict(example_measures)
+print(prediction)
