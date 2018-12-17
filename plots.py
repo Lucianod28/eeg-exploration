627cf5a29b3e30ef02771c6232f9876055c5351f
Lucianod28 <Lucianod28@gmail.com>
2018-12-17 18:39:32 -0300

    created SVM classifier

--- plots.py
+++ plots.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn import preprocessing, model_selection, neighbors, svm
+from scipy.signal import welch
 
 with open('SubjectMichelle___timestamp_1501862371.27/SubjectMichelle__log.txt') as f:
 	lines = [line.rstrip('\n').replace('\'', '"') for line in f]
@@ -15,46 +16,49 @@ for line in lines[1:]:
 df = pd.read_csv('SubjectMichelle___timestamp_1501862371.27/SubjectMichelle__eeg.csv', header=8)
 # df2 = pd.read_csv('SubjectMichelle___timestamp_1501862954.25/SubjectMichelle__eeg.csv', header=8)
 # df3 = pd.read_csv('SubjectMichelle___timestamp_1501863670.92/SubjectMichelle__eeg.csv', header=8)
-
-
+# Classify all the 
+# for index, row in df.iterrows():
+# 	one_sec = df.O1[:500]
+df.drop(['Channel Names'], 1, inplace=True) # drop first
 # Label left intervals with -1, right with 1, and rest with 0
-for trial in trials:
-	conditions = [(df['\t'] >= trial[0]) & (df['\t'] <= trial[1]), # left
-	(df['\t'] >= trial[2]) & (df['\t'] <= trial[3])] # right
-	choices = [-1, 1]
-	df['class'] = np.select(conditions, choices, default=0)
+# df['class'] = [0] * len(df.index)
+# for trial in trials:
+# 	conditions = [(df['\t'] >= trial[0]) & (df['\t'] <= trial[1]), # left
+# 	(df['\t'] >= trial[2]) & (df['\t'] <= trial[3])] # right
+# 	choices = [-1, 1]
+# 	df['class'] = np.select(conditions, choices, df['class'])
 
-# MAKE A LIST OF INTERVALS
-# Create a list of all the left light trials, next I will include the other 2 recordings above
-lefts = [df.loc[(df['\t'] >= trials[0][0]) & (df['\t'] <= trials[0][1])]]
+# make a list of intervals
+lefts = []
+rights = []
+rest1 = []
+rest2 = []
 for trial in trials[1:]:
 	lefts.append(df.loc[(df['\t'] >= trial[0]) & (df['\t'] <= trial[1])])
+	rights.append(df.loc[(df['\t'] >= trial[2]) & (df['\t'] <= trial[3])])
+	rest1.append(df.loc[(df['\t'] >= trial[4]) & (df['\t'] <= trial[5])])
+	rest2.append(df.loc[(df['\t'] >= trial[6]) & (df['\t'] <= trial[7])])
 
-left_df = pd.DataFrame()
-for left_interval in lefts:
-	length = len(left_interval)
-	left_df = left_df.append(left_interval.iloc[[length / 2]])
-print(left_df)
+def fourier(intervals, X, y, label):
+	for interval in intervals:
+		for i in range(0, len(interval.index) // 500):
+			one_sec = interval.O1[i * 500 : (i+1)*500]
+			X.append(np.concatenate(welch(one_sec, fs=500, nperseg=500)))
+			y.append(label)
 
-# plt.plot(trial1_left['\t'], trial1_left['\t[\'Fp1\''])
-# plt.show()
+X = []
+y = []
+fourier(lefts, X, y, -1)
+fourier(rights, X, y, 1)
+fourier(rest1, X, y, 0)
+fourier(rest2, X, y, 0)
+X = pd.DataFrame(X)
 
 # Train an svm
-df.drop(['Channel Names'], 1, inplace=True) # drop first
-#print(df.head())
-exit(0)
-X = np.array(df.drop(['class'], 1))
-y = np.array(df['class'])
-
-X_train, X_test, yTrain, yTest = model_selection.train_test_split(X, y, test_size=0.2)
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
 
 clf = svm.SVC()
-clf.fit(X_train, yTrain)
+clf.fit(X_train, y_train)
 
-accuracy = clf.score(X_test, yTest)
-print(accuracy)
-
-# example_measures = np.array([[4,2,1,1,1,2,3,2,1], [4,2,1,2,2,2,3,2,1]])
-# example_measures = example_measures.reshape(len(example_measures), -1)
-# prediction = clf.predict(example_measures)
-print(prediction)
+accuracy = clf.score(X_test, y_test)
+print('Accuracy: ', accuracy)
